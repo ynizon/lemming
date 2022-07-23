6edf74d6bc9fa2ae09d0ba0b7d51c26d4fa1ed3c
ynizon <ynizon@gmail.com>
2022-07-23 17:45:24 +0200

    Add custom map

---
+++ app/Models/Map.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use Auth;
+use App\Models\Game;
+
+class Map extends Model
+{
+    public $timestamps = false;
+
+    public function games()
+    {
+        return $this->belongsToMany(Game::class);
+    }
+
+    public function exportMap() {
+        $file = fopen(storage_path("maps/map".$this->id.".txt"), "w+");
+        fputs($file, $this->map);
+        fclose($file);
+    }
+
+
+}
